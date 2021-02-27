3c616a5cdc2770e60aa3b1efb25b6c380e05d5ae
pedroh12314 <pedroh.12314@gmail.com>
2021-02-26 22:23:49 -0300

    Revert "ADD imports components + fontawesome + bootstrap"
    This reverts commit dbc3bd0ad7872a8713c0646c82fc929ea7ff6604.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CreateYourTeamComponent } from './components/create-your-team/create-your-team.component';
+import { HomeComponent } from './screens/home/home.component';
+import { MyTeamsComponent } from './components/my-teams/my-teams.component';
+import { TopFiveComponent } from './components/top-five/top-five.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CreateYourTeamComponent,
+    HomeComponent,
+    MyTeamsComponent,
+    TopFiveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
