e662636172cb4ee6670b7e73a85504036154f481
pedroh12314 <pedroh.12314@gmail.com>
2021-02-27 22:31:55 -0300

    ADD fontaewsome + photos (assets)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { CreateYourTeamComponent } from './components/create-your-team/create-your-team.component';
 import { HomeComponent } from './screens/home/home.component';
 import { MyTeamsComponent } from './components/my-teams/my-teams.component';
 import { TopFiveComponent } from './components/top-five/top-five.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { InsightPlayersComponent } from './components/insight-players/insight-players.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CreateTeamComponent } from './screens/create-team/create-team.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +20,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     CreateYourTeamComponent,
     HomeComponent,
     MyTeamsComponent,
-    TopFiveComponent
+    TopFiveComponent,
+    InsightPlayersComponent,
+    FooterComponent,
+    CreateTeamComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     FontAwesomeModule
   ],
   providers: [],
