0ab980f35f9b5de363842dfe08e9300dff0dca51
pedroh12314 <pedroh.12314@gmail.com>
2021-03-02 14:00:21 -0300

    ADD insight component

--- src/app/components/insight-players/insight-players.component.ts
+++ src/app/components/insight-players/insight-players.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsightPlayersComponent implements OnInit {
 
+  name: string = "Pedro Henrique Lemes"
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getInitials(name){
+    const fullName = name.split(' ');
+    const initials = fullName.shift().charAt(0) + fullName.pop().charAt(0);
+    return initials.toUpperCase();
   }
 
 }
