73c5f2c0a81add941b0daffb9378faee79568490
pedroh12314 <pedroh.12314@gmail.com>
2021-03-04 20:52:22 -0300

    FIX top-five (add teams)

--- src/app/components/top-five/top-five.component.ts
+++ src/app/components/top-five/top-five.component.ts
@@ -5,11 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './top-five.component.html',
   styleUrls: ['./top-five.component.css']
 })
+
 export class TopFiveComponent implements OnInit {
+
+  highest: Team[]
+  lowest: Team[]
 
   constructor() { }
 
   ngOnInit(): void {
+    this.highest = [
+      {name: "Colombia", age: 28.6},
+      {name: "Argentina", age: 28.5},
+      {name: "Irã", age: 28.4},
+      {name: "Portugal", age: 28.1},
+      {name: "Uruguai", age: 28.0},
+    ]
+
+    this.lowest = [
+      {name: "Espanha", age: 27.7},
+      {name: "Itália", age: 27.3},
+      {name: "Brasil", age: 26.7},
+      {name: "Inglaterra", age: 26},
+      {name: "Alemanha", age: 25.8},
+    ]
   }
 
+}
+
+interface Team {
+  name: string,
+  age: number,
 }
